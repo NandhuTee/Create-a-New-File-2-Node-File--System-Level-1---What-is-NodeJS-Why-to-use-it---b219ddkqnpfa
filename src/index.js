b3188c4b862368e51b33ff6a5c28f697c4563641
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-05-26 23:26:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
-    //Write your code here
-    //Don't change function name
+   try {
+        // Write the new content to the file, overwriting any existing content
+        await fs.writeFile(fileName, fileContent, 'utf-8');
+    } catch (error) {
+        console.error("Error updating the file:", error);
+        throw error; // Rethrow the error after logging it
+    }
   
 };
 module.exports =  writeFile ;
+
